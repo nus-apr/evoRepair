36a261bd40ade053d428985381b86992d257778c
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-12-06 11:41:18 +0800

    add comments

--- app/repair.py
+++ app/repair.py
@@ -1,6 +1,33 @@
 from app import emitter, utilities, definitions, values
 
+"""
+This is the function to implement the interface with EvoRepair and ARJA(APR Tool)
 
-def generate():
+Expected Inputs
+@arg dir_src : directory of source files
+@arg dir_classes: directory of class files
+@arg dir_test: directory of test files
+@arg dir_deps: directory for dependencies
+
+Expected Output
+@output list of patches in JSON format [ 1: { path-file: "//"}]
+"""
+def generate(dir_src, dir_classes, dir_test, dir_deps, fix_loc):
+    dummy_arg_1 = ""
+    dummy_arg_2 = ""
     emitter.title("Generating Patches")
+    list_patches = []
+    # some processing
+
+    # example command generation
+    repair_command = "arja --something={} --something2={}".format(dummy_arg_1,
+                                                                  dummy_arg_2)
+
+    # invoke command
+    utilities.execute_command(repair_command)
+
+    # TODO: Implement process to invoke ARJA and generate patches
+
+
+    return list_patches
 
--- app/validator.py
+++ app/validator.py
@@ -1,6 +1,52 @@
 from app import emitter, utilities, definitions, values
 
+"""
+This is the function to implement the interfacing with UniAPR (optimized validation)
 
-def validate():
+Expected Input
+@arg list of patches
+@arg list of test cases
+
+Expected Output
+@output matrix of result for each test x patch
+@output sorted list of plausible patches
+@output ranked list of test-cases and their mutation score
+"""
+def validate(list_patches, list_tests):
+    plausible_patches = []
+    test_scores = dict()
+    validation_results = dict()
     emitter.title("Validating Generated Patches")
+
+    # TODO: Implement process to invoke UniAPR and validate patches
+
+    return plausible_patches, test_scores, validation_results
+
+
+
+"""
+This is the interface for EvoSuite
+# Expected Input
+# @arg class_name: target class name for testing
+# @arg class_path: path for the target build files
+
+# Expected Output
+# @output list of test-cases JSON format
+"""
+def generate_additional_test(class_name, class_path):
+    dummy_arg_1 = ""
+    emitter.title("Generating Patches")
+    list_tests = []
+    # some processing
+
+    # example command generation
+    generate_command = "evosuite --algorithm={}".format(dummy_arg_1)
+
+    # invoke command
+    utilities.execute_command(generate_command)
+
+    # TODO: Implement process to invoke EvoSuite and generate additional test-cases
+
+    return list_tests
+
 
