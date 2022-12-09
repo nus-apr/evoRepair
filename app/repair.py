85e7c3e3ca0b1d93d16261e261156b4eeb3bf93b
Ruan Haifeng <hruan@comp.nus.edu.sg>
2022-12-09 11:21:24 +0800

    use Arja to `generate()` patches

--- app/repair.py
+++ app/repair.py
@@ -1,33 +1,60 @@
 from app import emitter, utilities, values
 
+from os.path import exists, isdir
+import os
+import datetime
+
 """
 This is the function to implement the interface with EvoRepair and ARJA(APR Tool)
 
 Expected Inputs
 @arg dir_src : directory of source files
-@arg dir_classes: directory of class files
-@arg dir_test: directory of test files
+@arg dir_bin: directory of class files
+@arg dir_test_bin: directory of test files
 @arg dir_deps: directory for dependencies
+@arg dir_output_patches: directory for generated patches
 
 Expected Output
-@output list of patches in JSON format [ 1: { path-file: "//"}]
+@output list of paths of patch files ["/path/to/patch1", "/path/to/patch2"]
 """
+
+
 def generate():
-    dummy_arg_1 = ""
-    dummy_arg_2 = ""
     emitter.sub_sub_title("Generating Patches")
-    list_patches = []
-    # some processing
+
+    now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8)))
+    dir_chart_1_buggy = f"{values._dir_root}/test/chart_1_buggy"
+
+    dir_src = f"{dir_chart_1_buggy}/source"  # `dir_src` has to be absolute patch for Arja
+    dir_bin = f"{dir_chart_1_buggy}/build"
+    dir_test_bin = f"{dir_chart_1_buggy}/build-tests"
+    dir_deps = ""
+    dir_output_patches = f"{values.dir_tmp}/patches_chart_1_buggy_{now.strftime('%d%b%H:%M:%S')}"
+
+    if exists(dir_output_patches):
+        if not isdir(dir_output_patches):
+            emitter.error(f"{dir_output_patches} is not a directory")
+            return []
+        elif os.listdir(dir_output_patches):
+            emitter.warning(f"Output directory {dir_output_patches} is not empty; patch generation aborted")
+            return []
+
     emitter.normal("\trunning ARJA")
-    # example command generation
-    repair_command = "arja --something={} --something2={}".format(dummy_arg_1,
-                                                                  dummy_arg_2)
+    arja_command = (f'java -cp extern/arja/target/Arja-0.0.1-SNAPSHOT-jar-with-dependencies.jar'
+                    f' us.msu.cse.repair.Main Arja'
+                    f' -DsrcJavaDir "{dir_src}" -DbinJavaDir "{dir_bin}"'
+                    f' -DbinTestDir "{dir_test_bin}" -Ddependences "{dir_deps}"'
+                    f' -DpatchOutputRoot "{dir_output_patches}"'
+                    f' -DdiffFormat -DmaxGenerations 10'
+                    f' -DexternalProjRoot {values._dir_root}/extern/arja/external'
+                    )
+    arja_return_code = utilities.execute_command(arja_command)
 
-    # invoke command
-    # utilities.execute_command(repair_command)
+    if arja_return_code != 0:
+        emitter.error("\tARJA did not exit normally; no patch generated")
+        return []
 
-    # TODO: Implement process to invoke ARJA and generate patches
+    if not exists(dir_output_patches):
+        return []
 
-
-    return list_patches
-
+    return [patch.path for patch in os.scandir(dir_output_patches)]
