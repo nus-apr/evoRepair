8c8bc9c33cee657874b59c464689818954403b80
crhf <ruanhf8@gmail.com>
2023-04-28 23:40:32 +0000

    starter script

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+if [ ! "$#" -eq 1 ]; then
+	>&2 printf '%s\n' "Expected random seed"
+	exit 1
+fi
+
+seed="$1"
+
+BUGS=(math_95_instr lang_59_instr chart_1_instr math_50_instr math_70_instr math_73_instr)
+
+for bug in ${BUGS[@]}; do
+	./run_ablation.py new-ablation "$bug,$seed" &
+	sleep 8
+done
+
